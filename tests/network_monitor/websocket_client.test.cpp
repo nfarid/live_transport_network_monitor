69503590ac377d3fb391c3bcf47febdc1bfa9a0e
nfarid <54642193+nfarid@users.noreply.github.com>
2023-05-08 09:07:52 +0100

    Edit test case for tls

--- tests/network_monitor/websocket_client.test.cpp
+++ tests/network_monitor/websocket_client.test.cpp
@@ -21,30 +21,46 @@ BOOST_AUTO_TEST_CASE(class_WebSocketClient)
 {
     const std::string url = "ltnm.learncppthroughprojects.com";
     const std::string endpoint = "/echo";
-    const std::string port = "80";
+    const std::string port = "443";
     const std::string message = "Hello WebSocket";
 
     asio::io_context ioc{};
+    asio::ssl::context tls{asio::ssl::context::tlsv12_client};
+    tls.load_verify_file(TEST_CACERT_PEM);
 
-    WebSocketClient client{url, endpoint, port, ioc};
+    WebSocketClient client{url, endpoint, port, ioc, tls};
 
-    const auto onSend = [](error_code ec) {
-        BOOST_TEST(ec, "Sending error");
+    bool isSent = false;
+    bool isConnected = false;
+    bool isClosed = false;
+    bool isReceived = false;
+    std::string receivedMessage{};
+
+    const auto onSend = [& isSent](error_code ec) {
+        isSent = !ec;
     };
-    const auto onConnect = [& onSend, & message, & client](error_code ec) {
-        BOOST_TEST(ec, "Connection error");
+    const auto onConnect = [& onSend, & message, & client, & isConnected](error_code ec) {
+        isConnected = !ec;
         if(!ec)
             client.send(message, onSend);
     };
-    const auto onClose = [](error_code ec) {
-        BOOST_TEST(ec, "Closing error");
+    const auto onClose = [& isClosed](error_code ec) {
+        isClosed = !ec;
     };
-    const auto onReceive = [& client, & onClose, & message](error_code ec, std::string&& received) {
-        BOOST_TEST(ec, "Receiving error");
-        BOOST_TEST(message == received, "Received incorrect message");
+    const auto onReceive = [& client, & onClose, & isReceived, & receivedMessage](error_code ec, std::string&& received) {
+        isReceived = !ec;
+        receivedMessage = std::move(received);
         client.close(onClose);
     };
 
+    client.connect(onConnect, onReceive);
+    ioc.run();
+
+    BOOST_TEST(isSent);
+    BOOST_TEST(isConnected);
+    BOOST_TEST(isClosed);
+    BOOST_TEST(isReceived);
+    BOOST_TEST(receivedMessage == message);
 }
 
 BOOST_AUTO_TEST_SUITE_END(); //network_monitor
