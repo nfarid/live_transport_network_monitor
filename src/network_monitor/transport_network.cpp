4d909c104f527511b00a99b97277a96590f9a74e
nfarid <54642193+nfarid@users.noreply.github.com>
2023-05-12 17:20:49 +0100

    Implemented fromJson

--- src/network_monitor/transport_network.cpp
+++ src/network_monitor/transport_network.cpp
@@ -1,6 +1,8 @@
 
 #include <network_monitor/transport_network.hpp>
 
+#include <nlohmann/json.hpp>
+
 #include <cstddef>
 #include <set>
 #include <stdexcept>
@@ -10,8 +12,51 @@ using std::size_t;
 namespace NetworkMonitor {
 
 bool TransportNetwork::fromJson(nlohmann::json&& src) {
-    //TODO: Implement
-    return false;
+    m_nodeMp = {};
+
+    const auto& stations = src.at("stations");
+    for(const auto& stationJson : stations) {
+        const Station station{
+            .id = stationJson.at("station_id"),
+            .name = stationJson.at("name"),
+        };
+        if(!addStation(station) )
+            throw std::runtime_error("Unable to add station: " + station.id);
+    }
+
+    const auto& lines = src.at("lines");
+    for(const auto& lineJson : lines) {
+        Line line{
+            .id = lineJson.at("line_id"),
+            .name = lineJson.at("name"),
+        };
+        const auto& lineJsonRoutes = lineJson.at("routes");
+        for(const auto& routeJson : lineJsonRoutes) {
+            Route route {
+                .id = routeJson.at("route_id"),
+                .direction = routeJson.at("direction"),
+                .startStationId = routeJson.at("start_station_id"),
+                .endStationId = routeJson.at("end_station_id"),
+            };
+            const auto& routeStops = routeJson.at("route_stops");
+            for(auto& station : routeStops) {
+                route.stops.push_back(station);
+            }
+            line.routes.push_back(std::move(route) );
+        }
+        if(!addLine(line) )
+            throw std::runtime_error("Unable to add line: " + line.id);
+    }
+
+    const auto travelTimes = src.at("travel_times");
+    for(const auto& travelTime : travelTimes) {
+        const Id stationA = travelTime.at("start_station_id");
+        const Id stationB = travelTime.at("end_station_id");
+        const unsigned int time = travelTime.at("travel_time");
+        if(!setTravelTime(stationA, stationB, time) )
+            return false;
+    }
+    return true;
 }
 
 bool TransportNetwork::addStation(const Station& station) {
