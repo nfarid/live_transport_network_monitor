585dcf1304805468ea0f687d9cd69f2ec610b973
nfarid <54642193+nfarid@users.noreply.github.com>
2023-05-14 11:22:53 +0100

    Improved MockResolver

--- tests/network_monitor/boost_mock.hpp
+++ tests/network_monitor/boost_mock.hpp
@@ -8,7 +8,10 @@
 #include <boost/asio/post.hpp>
 
 #include <functional>
+#include <stdexcept>
 #include <string_view>
+
+namespace Mock {
 
 /*! \brief Mocks the case where DNS resolution fails.
  */
@@ -18,24 +21,33 @@ class MockResolver {
     using results_type = boost::asio::ip::tcp::resolver::results_type;
     using ResolveHandler = std::function<void(error_code, results_type)>;
 public:
+    /*! \brief Use this static member in a test to set the error code returned by async_resolve.
+     */
+    inline static error_code s_resolveEc;
+
     explicit MockResolver(io_context& ioc_) :
         m_ioc{ioc_}
     {}
 
-   void async_resolve(std::string_view, std::string_view, ResolveHandler&& handler) {
-       boost::asio::async_initiate<ResolveHandler, void(const error_code&, results_type)>(
-           [this](auto&& handler, auto resolver) {
-               boost::asio::post(m_ioc, [handler](){
-                   handler(boost::asio::error::operation_aborted, results_type{});
-               });
-           },
-           handler,
-           this
-       );
-   }
+    void async_resolve(std::string_view, std::string_view, ResolveHandler&& handler) {
+        boost::asio::async_initiate<ResolveHandler, void(const error_code&, results_type)>(
+            [](auto&& handler, auto resolver) {
+                if(s_resolveEc) {
+                    const auto failHander = [handler](){handler(s_resolveEc, results_type{}); };
+                    boost::asio::post(resolver->m_ioc, failHander);
+                } else {
+                    throw std::logic_error("MockResolver's successful branch is not implemented");
+                }
+            },
+            handler,
+            this
+        );
+    }
 
 private:
    io_context& m_ioc;
 };
+
+} //namespace Mock
 
 #endif //HPP_TEST_NETWORK_MONITOR_BOOSTMOCK_
